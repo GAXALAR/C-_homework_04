4ff7486123e562a12ef4057e2d6275b6e80dde83
GAXALAR <ay.kosyrev@gmail.com>
2023-01-25 18:48:46 +0300

    задача 27

--- домашние задания/языкиДЗ4/task_02/Program.cs
+++ домашние задания/языкиДЗ4/task_02/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+  int SumNumber(int num){
+    
+    int counter = Convert.ToString(num).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++){
+      advance = num - num % 10;
+      result = result + (num - advance);
+      num = num / 10;
+    }
+   return result;
+  }
+
+int sumNumber = SumNumber(num);
+Console.WriteLine("Сумма цифр в числе: " + sumNumber);
