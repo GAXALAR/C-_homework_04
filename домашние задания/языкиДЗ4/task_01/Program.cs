f129588211c7de038df9c07d106222da5ca291a5
GAXALAR <ay.kosyrev@gmail.com>
2023-01-25 18:36:46 +0300

    цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.

---
+++ домашние задания/языкиДЗ4/task_01/Program.cs
@@ -0,0 +1,17 @@
+﻿int Exponentiation(int num_a, int num_b)
+{
+  int result = 1;
+  for(int i=1; i <= num_b; i++){
+    result = result * num_a;
+  }
+   
+    return result;
+}
+
+  Console.Write("Введите число A: ");
+  int num_a = Convert.ToInt32(Console.ReadLine());
+  Console.Write("Введите число B: ");
+  int num_b = Convert.ToInt32(Console.ReadLine());
+
+  int exponentiation = Exponentiation(num_a, num_b);
+  Console.WriteLine("Ответ: " + exponentiation);
