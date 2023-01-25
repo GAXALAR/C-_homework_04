2369178ed65b4855ce97d55b632e3cf08bdef708
GAXALAR <ay.kosyrev@gmail.com>
2023-01-25 19:13:55 +0300

    задача 29

---
+++ домашние задания/языкиДЗ4/task_03/Program.cs
@@ -0,0 +1,7 @@
+﻿int [] array = new int [ 8 ];
+Console.WriteLine($"длина масива = { array.Length}");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 10);
+    Console.Write($"{array[i]},");
+}
